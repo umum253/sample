960f5392277db5f3618d1a53d19b4f95c2e2e242
umum253 <fkwpostpost@gmail.com>
2023-04-27 16:41:39 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors';
+import { createRouter } from './router';
+
+const app: Application = express()
+const PORT = 3000
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/', createRouter());
+
+app.get('/', async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  })
+})
+
+try {
+  app.listen(PORT, () => {
+    console.log(`dev server running at: http://localhost:${PORT}/`)
+  })
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message)
+  }
+}
+
+
---
+++ src/router.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+
+// 型定義
+type Data = {
+  user_id: string;
+  user_name: string;
+}
+
+// URLレスポンスデータ定義
+const userData: Data[] = [
+  {
+    "user_id": "001",
+    "user_name": "Yamada Taro"
+  },
+  {
+    "user_id": "002",
+    "user_name": "Tanaka Jiro"
+  }
+]
+
+export const createRouter = () => {
+  const router = Router();
+
+  router.get('/users', (_req, res) => {
+    res.status(200).send(userData);
+  });
+
+  return router;
+};
